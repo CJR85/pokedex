58b935054ef95c7b0e83ce57ac61a447c149c1d4
Calvin Roberts <roberts.calvin85@gmail.com>
2021-03-18 17:37:02 +0000

    Fetching Pokemon Data.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-'use-strict';
+const poke_container = document.getElementById('poke-container'),
+  poke_count = 150,
+  colors = {
+    fire: '#fddfdf',
+    grass: '#defde0',
+    electric: '#fcf7de',
+    water: '#def3fd',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#f5f5f5',
+    fighting: '#e6e0d4',
+    normal: '#f5f5f5',
+  };
+
+const fetchPokemons = async () => {
+  for (let i = 1; i <= poke_count; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`,
+    res = await fetch(url),
+    data = await res.json();
+  console.log(data);
+};
+
+fetchPokemons();
